4ab2d71d1d45e1e9024258027d3f3a35e0c05245
ShiJianXi <118092801+ShiJianXi@users.noreply.github.com>
2025-01-15 21:31:43 +0800

    Add line and indentation

--- src/main/java/ChitChatBot.java
+++ src/main/java/ChitChatBot.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 public class ChitChatBot {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         String name = "ChitChatBot";
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println("");
+        String line = "_____________________________________________________";
+        String indentation = "    ";
+        System.out.println(indentation + line);
+        System.out.println(indentation + "Hello! I'm " + name);
+        System.out.println(indentation + "What can I do for you?");
+        System.out.println(indentation + line);
         while (sc.hasNext()) {
             String input = sc.next();
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(indentation + line);
+                System.out.println(indentation + "Bye. Hope to see you again soon!");
+                System.out.println(indentation + line);
                 break;
             } else {
-                System.out.println(input);
+                System.out.println(indentation + line);
+                System.out.println(indentation + input);
+                System.out.println(indentation + line);
             }
         }
     }
