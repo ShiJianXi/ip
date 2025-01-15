4bb06ff029ab085d3e528d44286e6433a8c54fd8
ShiJianXi <118092801+ShiJianXi@users.noreply.github.com>
2025-01-15 20:22:55 +0800

    Complete level-1

--- src/main/java/ChitChatBot.java
+++ src/main/java/ChitChatBot.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class ChitChatBot {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String name = "ChitChatBot";
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
-        System.out.println(" ");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("");
+        while (sc.hasNext()) {
+            String input = sc.next();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
