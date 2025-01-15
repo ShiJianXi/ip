a6d5bd3f866fa30aac1018c052025dddf4a4458a
ShiJianXi <118092801+ShiJianXi@users.noreply.github.com>
2025-01-15 20:12:53 +0800

    Complete level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# ChitChatBot User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/ChitChatBot.java
@@ -0,0 +1,9 @@
+public class ChitChatBot {
+    public static void main(String[] args) {
+        String name = "ChitChatBot";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(" ");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
