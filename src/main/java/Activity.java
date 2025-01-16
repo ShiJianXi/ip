7f15b25930bc9ea444aa1f911d95de784ffe1129
ShiJianXi <118092801+ShiJianXi@users.noreply.github.com>
2025-01-16 12:32:08 +0800

    Complete level-2 and removed unnecessary fields in activity

--- src/main/java/Activity.java
+++ src/main/java/Activity.java
@@ -1,14 +1,8 @@
 public class Activity {
     private String name;
-    private static int noOfActivity;
 
     public Activity(String name) {
         this.name = name;
-        noOfActivity++;
-    }
-
-    public int getNoOfActivity() {
-        return noOfActivity;
     }
 
     @Override
