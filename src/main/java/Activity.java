fcb8e1ded4177e45368bb1bb901fd2f44e5cde39
ShiJianXi <118092801+ShiJianXi@users.noreply.github.com>
2025-01-16 15:48:21 +0800

    Complete level-3

--- src/main/java/Activity.java
+++ src/main/java/Activity.java
@@ -1,12 +1,36 @@
 public class Activity {
     private String name;
+    private boolean isDone;
+    private int index;
+    private static int noOfActivity = 0;
 
     public Activity(String name) {
         this.name = name;
+        this.isDone = false;
+        noOfActivity++;
+        this.index = noOfActivity;
+    }
+
+    public static int getNoOfActivity() {
+        return noOfActivity;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public void toggleIsDone() {
+        this.isDone = !isDone;
     }
 
     @Override
     public String toString() {
-        return name;
+        String string = "[%s] %s";
+        String toReturn = String.format(string, isDone == true ? "X" : " ", this.name);
+        return toReturn;
     }
 }
--- src/main/java/ChitChatBot.java
+++ src/main/java/ChitChatBot.java
@@ -18,19 +18,35 @@ public class ChitChatBot {
         //Takes in user input
         while (sc.hasNext()) {
             String input = sc.nextLine();
-            if (input.equals("bye")) {
+            String[] inputArr = input.split(" ");
+            if (inputArr[0].equals("bye")) {
                 System.out.println(indentation + line);
                 System.out.println(indentation + "Bye. Hope to see you again soon!");
                 System.out.println(indentation + line);
                 break;
-            } else if (input.equals("list")){
-                int len = activities.size();
+            } else if (inputArr[0].equals("list")){
                 System.out.println(indentation + line);
-                for (int i = 0; i < len; i++) {
-                    String activityString = ". " + activities.get(i).toString();
+                for (int i = 0; i < Activity.getNoOfActivity(); i++) {
                     int index = i + 1;
-                    System.out.println(indentation + index + activityString);
+                    System.out.println(indentation + index + "."
+                            + activities.get(i).toString());
                 }
+                System.out.println(indentation + line);
+            } else if (inputArr[0].equals("mark")){
+                int index = Integer.parseInt(inputArr[1]) - 1;
+                Activity targetedActivity = activities.get(index);
+                targetedActivity.markAsDone();
+                System.out.println(indentation + line);
+                System.out.println(indentation + "Nice! I've marked this task as done");
+                System.out.println(indentation + "  " + targetedActivity);
+                System.out.println(indentation + line);
+            } else if (inputArr[0].equals("unmark")){
+                int index = Integer.parseInt(inputArr[1]) - 1;
+                Activity targetedActivity = activities.get(index);
+                targetedActivity.markAsNotDone();
+                System.out.println(indentation + line);
+                System.out.println(indentation + "OK, I've marked this task as not done yet:");
+                System.out.println(indentation + "  " + targetedActivity);
                 System.out.println(indentation + line);
             } else {
                 String add = "added: ";
