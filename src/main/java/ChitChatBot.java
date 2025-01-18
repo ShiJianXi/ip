e3b6b6a1ee823568c5e6ab2fcf73c015cd0e70f8
ShiJianXi <118092801+ShiJianXi@users.noreply.github.com>
2025-01-18 21:28:40 +0800

    Add exception handling for unmark queries

--- src/main/java/ChitChatBot.java
+++ src/main/java/ChitChatBot.java
@@ -9,8 +9,7 @@ public class ChitChatBot {
         ArrayList<Task> Tasks = new ArrayList<>();
 
         //Greet the user
-        System.out.println(printChat(indentation + "Hello! I'm " + name + "\n"
-                + indentation + "What can i do for you?" + "\n"));
+        System.out.println(printChat(indentation + "Hello! I'm " + name + "\n" + indentation + "What can i do for you?" + "\n"));
 
         //Takes in user input
         while (sc.hasNext()) {
@@ -32,38 +31,43 @@ public class ChitChatBot {
 
             } else if (inputArr[0].equals("mark")) {
                 try {
+
                     int index = Integer.parseInt(inputArr[1]) - 1;
                     Task targetedTask = Tasks.get(index);
                     targetedTask.markAsDone();
 
-                    System.out.println(printChat(indentation + "Nice! I've marked this task as done:\n"
-                            + indentation + "  " + targetedTask + "\n"));
+                    System.out.println(printChat(indentation + "Nice! I've marked this task as done:\n" + indentation + "  " + targetedTask + "\n"));
                 } catch (IndexOutOfBoundsException e) {
-                    int index = Integer.parseInt(inputArr[1]) - 1;
                     if (Task.getNoOfActivity() == 0) {
-                        System.out.println(printChat(indentation
-                                + "Unable to mark, no task in the list, please add task first\n"));
+                        System.out.println(printChat(indentation + "Unable to mark, no task in the list, please add task first\n"));
                     } else if (Task.getNoOfActivity() == 1) {
-                        System.out.println(printChat(indentation
-                                + "This task doesn't exist, only 1 task in the list\n"));
+                        System.out.println(printChat(indentation + "This task doesn't exist, only 1 task in the list\n"));
                     } else {
-                        System.out.println(printChat(indentation
-                                + "This task doesn't exist, please pick a task from 1 to "
-                                + Task.getNoOfActivity() + "\n"));
+                        System.out.println(printChat(indentation + "This task doesn't exist, please pick a task from 1 to " + Task.getNoOfActivity() + "\n"));
                     }
                 } catch (NumberFormatException e) {
-                    System.out.println(printChat(indentation
-                            + "ERROR: Please enter the number of the task that you want to mark\n"));
+                    System.out.println(printChat(indentation + "ERROR: Please enter the number of the task that you want to mark\n"));
                 }
 
             } else if (inputArr[0].equals("unmark")) {
-                int index = Integer.parseInt(inputArr[1]) - 1;
-                Task targetedTask = Tasks.get(index);
-                targetedTask.markAsNotDone();
+                
+                try {
+                    int index = Integer.parseInt(inputArr[1]) - 1;
+                    Task targetedTask = Tasks.get(index);
+                    targetedTask.markAsNotDone();
 
-                System.out.println(printChat(indentation + "OK, I've marked this task as not done yet:\n"
-                        + indentation + "  " + targetedTask + "\n"));
-
+                    System.out.println(printChat(indentation + "OK, I've marked this task as not done yet:\n" + indentation + "  " + targetedTask + "\n"));
+                } catch (IndexOutOfBoundsException e) {
+                    if (Task.getNoOfActivity() == 0) {
+                        System.out.println(printChat(indentation + "Unable to unmark, no task in the list, please add and mark task first\n"));
+                    } else if (Task.getNoOfActivity() == 1) {
+                        System.out.println(printChat(indentation + "This task doesn't exist, only 1 task in the list\n"));
+                    } else {
+                        System.out.println(printChat(indentation + "This task doesn't exist, please pick a task from 1 to " + Task.getNoOfActivity() + "\n"));
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println(printChat(indentation + "ERROR: Please enter the number of the task that you want to mark\n"));
+                }
             } else if (inputArr[0].equals("todo")) {
 
                 try {
@@ -71,12 +75,9 @@ public class ChitChatBot {
                     Tasks.add(newTask);
 
                     int noOfTasks = Task.getNoOfActivity();
-                    System.out.println(printChat(indentation + "Got it. I've added this task:\n"
-                            + indentation + "  " + newTask + "\n"
-                            + indentation + "Now you have " + noOfTasks + " tasks in the list.\n"));
+                    System.out.println(printChat(indentation + "Got it. I've added this task:\n" + indentation + "  " + newTask + "\n" + indentation + "Now you have " + noOfTasks + " tasks in the list.\n"));
                 } catch (EmptyParameterException e) {
-                    System.out.println(printChat(indentation +
-                            e.getMessage()));
+                    System.out.println(printChat(indentation + e.getMessage()));
                 }
 
             } else if (inputArr[0].equals("deadline")) {
@@ -84,29 +85,18 @@ public class ChitChatBot {
                 Task newTask = Deadline.createDeadline(inputArr);
                 Tasks.add(newTask);
                 int noOfTasks = Task.getNoOfActivity();
-                System.out.println(printChat(indentation + "Got it. I've added this task:\n"
-                        + indentation + "  " + newTask + "\n"
-                        + indentation + "Now you have " + noOfTasks + " tasks in the list.\n"));
+                System.out.println(printChat(indentation + "Got it. I've added this task:\n" + indentation + "  " + newTask + "\n" + indentation + "Now you have " + noOfTasks + " tasks in the list.\n"));
 
             } else if (inputArr[0].equals("event")) {
 
                 Task newTask = Event.createEvent(inputArr);
                 Tasks.add(newTask);
                 int noOfTasks = Task.getNoOfActivity();
-                System.out.println(printChat(indentation + "Got it. I've added this task:\n"
-                        + indentation + "  " + newTask + "\n"
-                        + indentation + "Now you have "
-                        + noOfTasks + " tasks in the list.\n"));
+                System.out.println(printChat(indentation + "Got it. I've added this task:\n" + indentation + "  " + newTask + "\n" + indentation + "Now you have " + noOfTasks + " tasks in the list.\n"));
 
             } else {
 
-                System.out.println(printChat(indentation
-                        + "OOPS!!! I'm sorry, but I don't know what that means :-(\n"
-                        + indentation + "Please use the correct queries:\n"
-                        + indentation + "todo <description>\n"
-                        + indentation + "deadline <description> /by <Date/Time>\n"
-                        + indentation + "event <description> /from <Date/Time> /to <Date/Time>\n"
-                        + indentation + "or list to show all the task\n"));
+                System.out.println(printChat(indentation + "OOPS!!! I'm sorry, but I don't know what that means :-(\n" + indentation + "Please use the correct queries:\n" + indentation + "todo <description>\n" + indentation + "deadline <description> /by <Date/Time>\n" + indentation + "event <description> /from <Date/Time> /to <Date/Time>\n" + indentation + "or list to show all the task\n"));
             }
         }
     }
