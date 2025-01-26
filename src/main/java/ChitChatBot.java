f39cde83a04b525217134de3a2a43dcda704d68b
ShiJianXi <118092801+ShiJianXi@users.noreply.github.com>
2025-01-27 00:52:23 +0800

    Complete A-MoreOOP

--- src/main/java/ChitChatBot.java
+++ src/main/java/ChitChatBot.java
@@ -1,40 +1,36 @@
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.Scanner;
 
 
 public class ChitChatBot {
+    private Path path;
+    private Storage storage;
 
-    public static void main(String[] args) throws MissingParameterException {
-
-        Scanner sc = new Scanner(System.in);
-        String name = "ChitChatBot";
-
-        //Get the path to create where the chat.txt supposed to be
-        Path path = Paths.get("data", "chat.txt");
-        Storage storage = new Storage(path);
-
+    public ChitChatBot(Path path) {
+        this.path = path;
+        this.storage = new Storage(path);
         storage.initStorage();
+    }
 
-        //Greet the user
-        System.out.println(Ui.printChat(Ui.indentation + "Hello! I'm "
-                + name + "\n"
-                + Ui.indentation + "What can i do for you?" + "\n"));
-
-        //Takes in user input
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        Ui.greetUser();
         while (sc.hasNext()) {
             String input = sc.nextLine();
             String[] inputArr = input.split(" ");
-            Action action = null;
 
             Parser parser = new Parser(inputArr, storage);
             parser.parseCommand();
         }
+    }
+
+    public static void main(String[] args) throws MissingParameterException {
+
+        //Get the path to create where the chat.txt supposed to be
+        Path path = Paths.get("data", "chat.txt");
+        ChitChatBot chitChatBot = new ChitChatBot(path);
+        chitChatBot.run();
+
     }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,4 +1,5 @@
 public class Ui {
+
     //Format for printing of message
     public static String printChat(String message) {
         String line = "_____________________________________________________";
@@ -7,4 +8,10 @@ public class Ui {
     }
 
     public static String indentation = "    ";
+
+    public static void greetUser() {
+        System.out.println(Ui.printChat(Ui.indentation + "Hello! I'm "
+                + "ChitChatBot" + "\n"
+                + Ui.indentation + "What can i do for you?" + "\n"));
+    }
 }
